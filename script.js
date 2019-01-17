e9b3f319a6c4be1469c7a20a60fb674b6c342b7b
Chani Mores <32770649+chanimores@users.noreply.github.com>
2019-01-17 13:45:26 -0500

    Cleaned up file and changed molecules to use Class format

--- script.js
+++ script.js
@@ -70,6 +70,10 @@ var port2Open = false;
 var timerOn = false;
 var timerTicking;
 
+/********************************************
+            DRAWING FUNCTIONS
+********************************************/
+
 // Draws a circle
 function drawCircle(x, y, radius, color) {
     ctx.beginPath();
@@ -99,86 +103,6 @@ function drawArc(x1, y1, x2, y2, x3, y3, color) {
     ctx.lineWidth = 6;
     ctx.stroke();
     ctx.lineWidth = 1;
-}
-
-// Change on input function for molecule 1 slider
-mole1Slider.oninput = function() {
-    // Set molecule 1 radius based off of slider
-    mole1Radius = mole1Slider.value;
-    // Set molecule 1 radius display text based off of slider
-    mole1Span.textContent = mole1Slider.value;
-    // Update canvas if needed
-    updateDrawing();
-};
-
-// Change on input function for molecule 2 slider
-mole2Slider.oninput = function() {
-    // Set molecule 2 radius based off of slider
-    mole2Radius = mole2Slider.value;
-    // Set molecule 2 radius display text based off of slider
-    mole2Span.textContent = mole2Slider.value;
-    // Update canvas if needed
-    updateDrawing();
-};
-
-// Draws a big red particle
-function drawBigParticle(x, y) {
-    // Linear gradient for style
-    let redGradient = ctx.createLinearGradient(x-75, 0, x+100, 0);
-    redGradient.addColorStop(0, "#540000");
-    redGradient.addColorStop(0.5, "red");
-    redGradient.addColorStop(1, "#ffac75");
-    
-    // Use radius from slider, starting value is 50
-    drawCircle(x, y, mole1Radius, redGradient);
-}
-
-// Draws a little particle
-function drawLilParticle(x, y) {
-    let darkGradient = ctx.createRadialGradient(x, y, mole2Radius*(2/3), x, y, mole2Radius*(2/15));
-    darkGradient.addColorStop(0, "#002602");
-    darkGradient.addColorStop(0.75, "#47772c"); // inside color
-    
-    // Use radius from slider, starting value is 10
-    drawCircle(x, y, mole2Radius, darkGradient);
-}
-
-/********************************************
-    Temperature Functions and Globals
-********************************************/
-
-// Adds/Subtracts 5 from temp
-// Speed is also changed here (is a fifth of temp)
-function minusTemp() {
-    if (temp > 25) {
-        temp = temp - 5;
-        speed = temp / 5;
-    }
-    tempSpan.textContent = temp;
-}
-function addTemp() {
-    if (temp < 75) {
-        temp = temp + 5;
-        speed = temp/5;
-    }
-    tempSpan.textContent = temp;
-}
-
-/********************************************
-    Active Transport Functions and Globals
-********************************************/
-
-function toggleAT() {
-    if (activeAT === false) {
-        activeAT = true;
-        spanAT.textContent = "Disable";
-    }
-    else {
-        activeAT = false;
-        spanAT.textContent = "Enable";
-    }
-    // Update canvas if needed
-    updateDrawing();
 }
 
 // Draws AT, with gradient if AT is enabled
@@ -224,107 +148,41 @@ function drawWall() {
     drawAT(canvasWidth/2, 450);
 }
 
-// Returns a negative or positive number for random movement
-function randomCo() {
-    // Speed + random value so there's always some movement
-    let distance = speed + (Math.random() * Math.floor(speed));
-    
-    // Picks a direction of negative or positive
-    let direction = (Math.random());
-    if (direction > 0.5) {
-        direction = 1;
+/********************************************
+            TOGGLES AND INPUT
+********************************************/
+
+// Change on input function for molecule 1 slider
+mole1Slider.oninput = function() {
+    // Set molecule 1 radius based off of slider
+    mole1Radius = mole1Slider.value;
+    // Set molecule 1 radius display text based off of slider
+    mole1Span.textContent = mole1Slider.value;
+    // Update canvas if needed
+    updateDrawing();
+};
+
+// Change on input function for molecule 2 slider
+mole2Slider.oninput = function() {
+    // Set molecule 2 radius based off of slider
+    mole2Radius = mole2Slider.value;
+    // Set molecule 2 radius display text based off of slider
+    mole2Span.textContent = mole2Slider.value;
+    // Update canvas if needed
+    updateDrawing();
+};
+
+function toggleAT() {
+    if (activeAT === false) {
+        activeAT = true;
+        spanAT.textContent = "Disable";
     }
     else {
-        direction = -1;
+        activeAT = false;
+        spanAT.textContent = "Enable";
     }
-    
-    return (distance * direction);
-}
-
-// Checks if the x and y coordinates are inside the canvas
-function isInCanvas(x, y) {
-    if (x > 0 && y > 0 && y < canvasHeight && x < canvasWidth) {
-        return true;
-    }
-    return false;
-}
-
-// Function to fill the coordinate arrays in an orderly fashion
-function particleStartLocations() {
-    if (timerOn === false) {
-        // Clear arrays
-        xCo = [];
-        yCo = [];
-        // Big particles
-        xCo = [160, 160];
-        yCo = [125, 375];
-        // Lil particles
-        // R for rows and C for columns
-        for (let r = 0; r < 10; r++) {
-            for (let c = 0; c < 7; c++) {
-                // Add lil particle coordinates to arrays
-                xCo.push(25 + (c * 45));
-                yCo.push(25 + (r * 50));
-                
-                // Remove lil particle coordinates if near big particle
-                if ((2 <= c && c <= 4) && ((1 <= r && r <= 3) || (6 <= r && r <= 8))) {
-                    xCo.pop();
-                    yCo.pop();
-                }
-            }
-        }
-        draw(); 
-    }
-}
-
-// Moves all the particles
-function moveParticles() {
-    let tempX, tempY;
-    for(let i = 0; i < xCo.length; i++) {
-        /* Add x y ranges for checks so lil particles far right/left don't have to go
-        through the checks for the wall/AT and particles high up won't have to go 
-        through checks for lower AT*/
-        
-        do {
-            // Picks a new location for the particle
-            tempX = xCo[i] + randomCo();
-            tempY = yCo[i] + randomCo();
-            // If new location is outside canvas, loop
-        } 
-        while (!isInCanvas(tempX, tempY));
-        
-        // Set particle coordinates to valid new location 
-        xCo[i] = tempX;
-        yCo[i] = tempY;
-    }
-}
-
-// Draws things
-function draw() {
-    // Resets canvas
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    
-    drawWall();
-
-    // Only move particles if timer is on
-    if (timerOn === true) {
-        moveParticles();
-    }
-    
-    // Draws the particles
-    drawBigParticle(xCo[0], yCo[0]);
-    drawBigParticle(xCo[1], yCo[1]);
-    for (let i = 2; i < xCo.length; i++) {
-        drawLilParticle(xCo[i], yCo[i]);
-    }
-}
-
-// Update canvas only if timer is not on, update if neeeded
-function updateDrawing() {
-    if (timerOn === false) {
-        draw();
-    }
+    // Update canvas if needed
+    updateDrawing();
 }
 
 // Toggles timer (and drawing) on/off
@@ -342,20 +200,6 @@ function toggleTimer() {
         clearInterval(timerTicking);
     }
 }
-
-// Tick() incriments the timer
-function tick() {
-    if (timerInput.value > 1) {
-        timerInput.value--;
-    }
-    else if (timerInput.value <= 1) {
-        timerInput.value = 0;
-        toggleTimer();
-    }
-}
-
-// Initally populate arrays with coordinates after timerOn exists
-particleStartLocations();
 
 // Toggles port 1 open/close
 function togglePort1() {
@@ -384,4 +228,189 @@ function togglePort2() {
     // Update canvas if needed
     updateDrawing();
 }
+
+/********************************************
+    Temperature Functions and Globals
+********************************************/
+
+// Adds/Subtracts 5 from temp
+// Speed is also changed here (is a fifth of temp)
+function minusTemp() {
+    if (temp > 25) {
+        temp = temp - 5;
+        speed = temp / 5;
+    }
+    tempSpan.textContent = temp;
+}
+function addTemp() {
+    if (temp < 75) {
+        temp = temp + 5;
+        speed = temp/5;
+    }
+    tempSpan.textContent = temp;
+}
+
+/********************************************
+    Active Transport Functions and Globals
+********************************************/
+
+    // Returns a negative or positive number for random movement
+    function randomCo() {
+        // Speed + random value so there's always some movement
+        let distance = speed + (Math.random() * Math.floor(speed));
+        
+        // Picks a direction of negative or positive
+        let direction = (Math.random());
+        if (direction > 0.5) {
+            direction = 1;
+        }
+        else {
+            direction = -1;
+        }
+        
+        return (distance * direction);
+    }
+
+        // Checks if the x and y coordinates are inside the canvas
+    function isInCanvas(x, y) {
+        if (x > 0 && y > 0 && y < canvasHeight && x < canvasWidth) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+class Molecule {
+
+    constructor(moleculeType, startX, startY) {
+        this.x = startX;
+        this.y = startY;
+    }
+
+    /************************************/
+
+    /************************************/
+
+    static isCollidingWithWallOrPort() {
+        // add code
+        return false;
+    }
+
+    static isCollidingWithAT() {
+        // add code
+        return false;
+    }
+
+    /************************************/
+
+    updateMolecule() {
+        let tempX, tempY;
+            /* Add x y ranges for checks so lil particles far right/left don't have to go
+            through the checks for the wall/AT and particles high up won't have to go 
+            through checks for lower AT*/
+            
+        do {
+                // Picks a new location for the particle
+                tempX = this.x + randomCo();
+                tempY = this.y + randomCo();
+                // If new location is outside canvas, loop
+        } 
+        while (!isInCanvas(tempX, tempY));
+            
+        // Set particle coordinates to valid new location 
+        this.x = tempX;
+        this.y = tempY;
+    }
+
+    // type is passed in because this.moleculeType was not working
+    draw(type) {
+        // Draws Large molecule type
+        if (type == 1) {
+            // Linear gradient for style
+            let gradient = ctx.createLinearGradient(this.x-75, 0, this.x+100, 0);
+            gradient.addColorStop(0, "#540000");
+            gradient.addColorStop(0.5, "red");
+            gradient.addColorStop(1, "#ffac75");
+            
+            drawCircle(this.x, this.y, mole1Radius, gradient);
+        } else {
+            // Draws Small molecule type
+            let gradient = ctx.createRadialGradient(this.x, this.y, mole2Radius*(2/3), this.x, this.y, mole2Radius*(2/15));
+            gradient.addColorStop(0, "#002602");
+            gradient.addColorStop(0.75, "#47772c"); // inside color
+            
+            drawCircle(this.x, this.y, mole2Radius, gradient);
+        }
+    }
+}
+
+// Function to fill the coordinate arrays in an orderly fashion
+function particleStartLocations() {
+    if (timerOn === false) {
+        // Clear array
+        molecules = [];
+
+        // puts the two big molecules in the first two slots
+        molecules = [new Molecule(1, 160, 125), new Molecule(1, 160, 375)];
+
+        // iterates over rows (r) and columns (c)
+        for (let r = 0; r < 10; r++) {
+            for (let c = 0; c < 7; c++) {
+                // adds a small molecule at a specific interval
+                molecules.push(new Molecule(2, (25 + (c * 45)), (25 + (r * 50))));
+
+                // removes small molecule if near the big one
+                if ((2 <= c && c <= 4) && ((1 <= r && r <= 3) || (6 <= r && r <= 8))) {
+                    molecules.pop();
+                }
+            }
+        }
+        draw(); 
+    }
+}
+
+// Draws things
+function draw() {
+    // Resets canvas
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    
+    drawWall();
+
+    // Iterates over all the molecules
+    for (let i = 0; i < molecules.length; i++) {
+        // updates movement if timer is on
+        if (timerOn === true) {
+            molecules[i].updateMolecule();
+        }
+
+        // draws them to the canvas
+        if (i < 2){
+            molecules[i].draw(1); // big molecules
+        } else {
+            molecules[i].draw(2); // small molecules
+        }
+    }
+}
+
+// Update canvas only if timer is not on, update if neeeded
+function updateDrawing() {
+    if (timerOn === false) {
+        draw();
+    }
+}
+
+// Tick() incriments the timer
+function tick() {
+    if (timerInput.value > 1) {
+        timerInput.value--;
+    }
+    else if (timerInput.value <= 1) {
+        timerInput.value = 0;
+        toggleTimer();
+    }
+}
+
+// Initally populate arrays with coordinates after timerOn exists
+particleStartLocations();
 
